3f1da33b6ea48ef4f504fde23ad2106f426251a6
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-16 15:59:26 +0100

    Added custom colours to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
   content: ["./src/**/**/**.{html,js,jsx}"],
   theme: {
     extend: {},
+    colors: {
+      dButton: "#bc2f6a",
+      accent: "#bc3b2f",
+
+      lPrimary: "#130710",
+      lBackground: "#f9f0f7",
+      lSecondaryButton: "#ffffff",
+
+      dPrimary: "#f7f3f3",
+      dBackground: "#181211",
+      dSecondaryButton: "231c1a"
+    }
   },
   plugins: [],
 }
