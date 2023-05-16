5fef522c8a03f71e7637c963d25ed776f15041d9
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-16 14:32:15 +0100

    Basic web structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export function Footer() {
+    return (
+        <footer>
+            <p>Made by</p>
+            <a href="https://github.com/OscarSoanes">Oscar Soanes</a>
+        </footer>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return (
+        <header>
+            <h1>Memory Card Game</h1>
+
+            <button>Dark Mode</button>
+        </header>
+    )
+}
