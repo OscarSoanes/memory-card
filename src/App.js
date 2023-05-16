1e60bbc62e9a12bcc803481a922061373b6ba376
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-16 14:47:58 +0100

    Added a dark mode toggle button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
+  }
+
   return (
-    <div className="App">
-      <Header />
+    <div className={darkMode && "dark"}>
+      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
       <Main />
       <Footer />
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
-export function Header() {
+import { BsFillMoonFill } from 'react-icons/bs';
+import { BsFillSunFill } from 'react-icons/bs';
+
+export function Header({darkMode, toggleDarkMode}) {
     return (
         <header>
             <h1>Memory Card Game</h1>
 
-            <button>Dark Mode</button>
+            <button onClick={toggleDarkMode}>
+                {darkMode ? <BsFillMoonFill /> : <BsFillSunFill />}
+            </button>
         </header>
     )
 }
