6dabf82b32ca2e69cab08fea9fa76272d747f541
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-16 16:35:14 +0100

    Added a gradient colour over text displaying the edition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
-import { BsFillMoonFill } from 'react-icons/bs';
-import { BsFillSunFill } from 'react-icons/bs';
+import { BsFillMoonFill } from "react-icons/bs";
+import { BsFillSunFill } from "react-icons/bs";
 
-export function Header({darkMode, toggleDarkMode}) {
-    return (
-        <header>
-            <h1>Memory Card Game</h1>
+export function Header({ darkMode, toggleDarkMode }) {
+  return (
+    <header>
+      <div>
+        <h1>Memory Card Game</h1>
+        <button onClick={toggleDarkMode}>
+          {darkMode ? <BsFillMoonFill /> : <BsFillSunFill />}
+        </button>
+      </div>
 
-            <button onClick={toggleDarkMode}>
-                {darkMode ? <BsFillMoonFill /> : <BsFillSunFill />}
-            </button>
-        </header>
-    )
+      <p className='font-bold bg-gradient-to-r from-button to-accent inline-block text-transparent bg-clip-text'>
+        Colour Edition
+      </p>
+    </header>
+  );
 }
