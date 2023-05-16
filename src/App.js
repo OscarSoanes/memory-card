6ff16cb1679d29493646e50ac83f34ca49c2c729
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-16 15:17:18 +0100

    Added an useEffect to get the last stored dark mode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
 
-  function toggleDarkMode() {
+  useEffect(() => {
+    // On page load
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      setDarkMode(true);
+    }
+  }, [])
+
+  function toggleDarkMode() {   
+    localStorage.theme = darkMode ? "light" : "dark";
     setDarkMode(!darkMode);
   }
 
   return (
-    <div className={darkMode && "dark"}>
+    <div className={darkMode ? "dark" : undefined}>
       <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
       <Main />
       <Footer />
