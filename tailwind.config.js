a16503237b60191aee494bd65d42992e86a9f598
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-16 16:34:47 +0100

    Made colours extend rather than overwrite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,20 @@ module.exports = {
   darkMode: 'class',
   content: ["./src/**/**/**.{html,js,jsx}"],
   theme: {
-    extend: {},
-    colors: {
-      dButton: "#bc2f6a",
-      accent: "#bc3b2f",
-
-      lPrimary: "#130710",
-      lBackground: "#f9f0f7",
-      lSecondaryButton: "#ffffff",
-
-      dPrimary: "#f7f3f3",
-      dBackground: "#181211",
-      dSecondaryButton: "231c1a"
-    }
+    extend: {
+      colors: {
+        button: "#bc2f6a",
+        accent: "#bc3b2f",
+  
+        lPrimary: "#130710",
+        lBackground: "#f9f0f7",
+        lSecondaryButton: "#ffffff",
+  
+        dPrimary: "#f7f3f3",
+        dBackground: "#181211",
+        dSecondaryButton: "231c1a"
+      }
+    },
   },
   plugins: [],
 }
