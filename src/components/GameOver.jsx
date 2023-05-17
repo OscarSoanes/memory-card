72473a11b624636a406a3ee23c3ac178a247e8c3
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-17 13:37:51 +0100

    Game over menu

---
+++ src/components/GameOver.jsx
@@ -0,0 +1,11 @@
+import { PrimaryButton } from "./PrimaryButton";
+
+export function GameOver({ result, onClick }) {
+  return (
+    <section className='mx-auto w-fit p-8 mt-8 rounded-md drop-shadow-md bg-lSecondaryButton dark:bg-dSecondaryButton flex flex-col items-center'>
+      <h2 className='text-xl sm:text-2xl'>You {result}!</h2>
+      <p className='mt-1 mb-4 sm:text-xl'>Want to play again?</p>
+      <PrimaryButton text='Play Again?' onClick={onClick} />
+    </section>
+  );
+}
