55aba7f13c7f7716071bb423a72070433e555ed8
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-17 11:32:55 +0100

    Styling the Cells colour

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,6 +1,9 @@
 export function Cell({ colour }) {
   return (
-    <section className={""} style={{ backgroundColor: colour.hex }}>
+    <section
+      className={"aspect-square rounded-md"}
+      style={{ backgroundColor: colour.hex }}
+    >
       <h2>{colour.hex}</h2>
       <p>{colour.name}</p>
     </section>
