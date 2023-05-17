039b979d5f84d871a480a8f32924e8248cab3bf6
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-17 02:09:29 +0100

    Styled the start screen and the primary button

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -1,6 +1,10 @@
 export function PrimaryButton({ text, onClick, icon }) {
   return (
-    <button onClick={onClick} value={text}>
+    <button
+      onClick={onClick}
+      value={text}
+      className='flex gap-2 items-center p-3 bg-button rounded  text-dPrimary'
+    >
       {icon}
       {text}
     </button>
--- src/components/StartMenu.jsx
+++ src/components/StartMenu.jsx
@@ -3,10 +3,10 @@ import { PrimaryButton } from "./PrimaryButton";
 import { BsStar, BsStarHalf, BsStarFill } from "react-icons/bs";
 export function StartMenu({ changeDifficulty }) {
   return (
-    <section>
-      <h2>Don't click on the same colour!</h2>
-      <p>Ready to Play?</p>
-      <div>
+    <section className='mx-auto block w-fit p-4 mt-8 rounded-xl drop-shadow-md bg-lSecondaryButton dark:bg-dSecondaryButton text-center'>
+      <h2 className='text-xl sm:text-2xl'>Don't click on the same colour!</h2>
+      <p className='mt-1 sm:text-xl'>Ready to Play?</p>
+      <div className='flex flex-col justify-center gap-4 mt-8 items-center sm:flex-row'>
         <PrimaryButton
           onClick={changeDifficulty}
           text='Easy'
