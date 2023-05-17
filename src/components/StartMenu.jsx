bca9cea53cbc77dbebc0df70062e3c7b3c4ac425
Oscar Soanes <oscarsoanes@gmail.com>
2023-05-17 02:19:24 +0100

    Modified start menu to have less rounded corners

--- src/components/StartMenu.jsx
+++ src/components/StartMenu.jsx
@@ -3,7 +3,7 @@ import { PrimaryButton } from "./PrimaryButton";
 import { BsStar, BsStarHalf, BsStarFill } from "react-icons/bs";
 export function StartMenu({ changeDifficulty }) {
   return (
-    <section className='mx-auto block w-fit p-4 mt-8 rounded-xl drop-shadow-md bg-lSecondaryButton dark:bg-dSecondaryButton text-center'>
+    <section className='mx-auto block w-fit p-4 mt-8 rounded-md drop-shadow-md bg-lSecondaryButton dark:bg-dSecondaryButton text-center'>
       <h2 className='text-xl sm:text-2xl'>Don't click on the same colour!</h2>
       <p className='mt-1 sm:text-xl'>Ready to Play?</p>
       <div className='flex flex-col justify-center gap-4 mt-8 items-center sm:flex-row'>
